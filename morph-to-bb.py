0eef3929da9ca5bf8e2da01054ee1f277d4819ae
Jonathan Maw <jonathan.maw@codethink.co.uk>
2016-06-27 17:11:22 +0100

    Parse chunks and add 'kind' checking

--- morph-to-bb.py
+++ morph-to-bb.py
@@ -11,22 +11,48 @@ recipes will be parsed from.
 Run this script from the root of the definitions directory.
     '''
 
+def parse_chunk(defs, chunk_data):
+    "Adds the chunk definition to defs if not already in"
+    chunks = defs['chunks']
+    chunk_path = chunk_data['morph']
+    if not chunk_path in chunks:
+        chunk = yaml.load(file(chunk_path, 'r'))
+        if chunk['kind'] != "chunk":
+            print chunk_path, "is not a chunk!"
+            sys.exit(1)
+
+        if chunk_data['name'] != chunk['name']:
+            print "Mismatched names in", chunk_path, "!"
+
+        chunks[chunk_path] = chunk
+
 def parse_stratum(defs, stratum_spec):
-    "Adds the stratum definition in stratum_spec.morph if not already in"
+    "Adds the stratum definition in stratum_spec to defs if not already in"
     strata = defs['strata']
     stratum_path = stratum_spec['morph']
 
     if not stratum_path in strata:
         stratum = yaml.load(file(stratum_path, 'r'))
+        if stratum['kind'] != "stratum":
+            print stratum_path, "is not a stratum!"
+            sys.exit(1)
+
         if stratum_spec['name'] != stratum['name']:
-            print "Mismatched names in", stratum_spec['morph'], "!"
+            print "Mismatched names in", stratum_path, "!"
+
         strata[stratum_path] = stratum
+        for chunk_data in stratum['chunks']:
+            parse_chunk(defs, chunk_data)
 
 
 def parse_system(defs, system_path):
     "Adds the system definition in system_path to defs if not already in"
     if not system_path in defs['systems']:
         system = yaml.load(file(system_path, 'r'))
+        if system['kind'] != "system":
+            print system_path, "is not a system!"
+            sys.exit(1)
+
         defs['systems'][system_path] = system
         for stratum_spec in system['strata']:
             parse_stratum(defs, stratum_spec)
