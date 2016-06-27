78b4ee9b32bbae286793f486d3bca2c97c7a88b4
Jonathan Maw <jonathan.maw@codethink.co.uk>
2016-06-27 15:38:48 +0100

    Add usage errors and vim modeline

--- morph-to-bb.py
+++ morph-to-bb.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import sys
+
+def print_usage():
+    usage = '''
+Usage: definitions_dir recipes_dir systems...
+Where definitions_dir is a directory that contains baserock definitions,
+recipes_dir is a directory that will be created and filled with bitbake recipes,
+and systems... is 1 or more systems identified by name, which recipes will be
+parsed from.
+    '''
 
 def main(argv):
     # Arg 1, a directory to find definitions
     # Arg 2, a directory to put recipes in
     # Arg 3..., Systems to parse
-    print "Arg 1 is", argv[0]
-    print "Arg 2 is", argv[1]
-    print "Other args are", argv[2:]
+
+    if len(argv) < 3:
+        print "Too few arguments"
+        print_usage()
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
